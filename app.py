9e4d78a4148421513bde8a8c30c18f298355be3c
Sonti Roy <sontiroy@gmail.com>
2024-04-25 23:56:08 +0530

    test

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
+picture = st.camera_input("Take a picture")
 
-# st.title('Hello World!')
-
-# st.write('This is a simple Streamlit app.')
-
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        # Perform any image processing operations here
-        return frame
-
-
-def main():
-    st.title("OpenCV with Streamlit-WebRTC")
-
-    webrtc_ctx = webrtc_streamer(
-        key="example",
-        video_transformer_factory=VideoTransformer,
-        async_transform=True,
-    )
-
-    if webrtc_ctx.video_transformer:
-        st.write("Webcam is running")
+if picture:
+    st.image(picture)
