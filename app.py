be4a46a3cf63ec66b8d846f32f7f971d139badda
Sonti Roy <sontiroy@gmail.com>
2024-04-26 23:35:31 +0530

    test app

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 import streamlit as st
 import numpy as np
+from PIL import Image
+
+
 # from skimage.io import imread
 # from skimage.transform import resize
 
 img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer:
-    st.image(img_file_buffer)
-    
-# flat_data_arr=[] #input array 
+# read the image and convert to array
 
-# if img_file_buffer is not None:
-#     # To read image file buffer with OpenCV:
-#     bytes_data = img_file_buffer.getvalue()
-#     # 
-#     img_array=imread(img_file_buffer) 
-#     img_resized=resize(img_array,(64,64,3)) 
-#     flat_data_arr.append(img_resized.flatten()) 
-#     flat_data=np.array(flat_data_arr) 
-#     print(img_array.shape)
-    
-#     st.write(img_array)
-#     st.write(flat_data)
+image = Image.open(img_file_buffer)
+image = np.array(image)
+
+st.image(image, caption="Captured Image", use_column_width=True)
+
+
+
     
