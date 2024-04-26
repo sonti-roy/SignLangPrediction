43f62e9011259fe80cd2af1a76531ffbc6b7e7b4
Sonti Roy <sontiroy@gmail.com>
2024-04-26 23:54:26 +0530

    test app

--- app.py
+++ app.py
@@ -9,13 +9,22 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
-
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
+    # transfor the array using pca to select 10 features
+    from sklearn.decomposition import PCA
+    num_features = 10  # Number of features to extract
+    pca = PCA(n_components=num_features)  # PCA for feature extraction
+    # Perform PCA to extract 10 features
+    pca.fit(cv2_img)
+    flat_data_pca = pca.transform(cv2_img)
+    
+    # load the model
+    import pickle
+    # load the model from disk
+    loaded_model = pickle.load(open('svm_model.sav', 'rb'))
+    # make a prediction
+    prediction = loaded_model.predict(flat_data_pca)
+    st.write(prediction)
+    
 
 
 
