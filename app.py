2a151ce507829ead485c26001fcae29d768e33d2
Sonti Roy <sontiroy@gmail.com>
2024-04-26 23:26:14 +0530

    app

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ img_file_buffer = st.camera_input("Take a picture")
 if img_file_buffer:
     st.image(img_file_buffer)
     
-flat_data_arr=[] #input array 
+# flat_data_arr=[] #input array 
 
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    # 
-    img_array=imread(img_file_buffer) 
-    img_resized=resize(img_array,(64,64,3)) 
-    flat_data_arr.append(img_resized.flatten()) 
-    flat_data=np.array(flat_data_arr) 
-    print(img_array.shape)
+# if img_file_buffer is not None:
+#     # To read image file buffer with OpenCV:
+#     bytes_data = img_file_buffer.getvalue()
+#     # 
+#     img_array=imread(img_file_buffer) 
+#     img_resized=resize(img_array,(64,64,3)) 
+#     flat_data_arr.append(img_resized.flatten()) 
+#     flat_data=np.array(flat_data_arr) 
+#     print(img_array.shape)
     
-    st.write(img_array)
-    st.write(flat_data)
+#     st.write(img_array)
+#     st.write(flat_data)
     
