18e019d64e09a5f680ebf82466e8dff4905cdcdc
Sonti Roy <sontiroy@gmail.com>
2024-04-27 00:57:56 +0530

    Add image removal functionality in app.py

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ img_file_buffer = st.camera_input("Take a picture")
 if img_file_buffer is not None:
     
     img = Image.open(img_file_buffer)
+    
+    img = remove(img) 
 
     # To convert PIL Image to numpy array:
     img_array = np.array(img)
