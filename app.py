95c10615b79780518945cb848e89972e5fd53826
Sonti Roy <sontiroy@gmail.com>
2024-04-27 00:23:18 +0530

    just testing

--- app.py
+++ app.py
@@ -16,21 +16,24 @@ if img_file_buffer is not None:
     flat_data=np.array(flat_data) 
     # reshape to 2D array
     flat_data = flat_data.reshape(1,-1)
+    
+    # select only 10 features from 0 to 10
+    flat_data = flat_data[:,0:10]
 
-    # transfor the array using pca to select 10 features
-    from sklearn.decomposition import PCA
-    num_features = 10  # Number of features to extract
-    pca = PCA(n_components=num_features)  # PCA for feature extraction
-    # Perform PCA to extract 10 features
-    pca.fit(flat_data)
-    flat_data_pca = pca.transform(flat_data)
+    # # transfor the array using pca to select 10 features
+    # from sklearn.decomposition import PCA
+    # num_features = 10  # Number of features to extract
+    # pca = PCA(n_components=num_features)  # PCA for feature extraction
+    # # Perform PCA to extract 10 features
+    # pca.fit(flat_data)
+    # flat_data_pca = pca.transform(flat_data)
     
     # load the model
     import pickle
     # load the model from disk
     loaded_model = pickle.load(open('svm_model.sav', 'rb'))
     # make a prediction
-    prediction = loaded_model.predict(flat_data_pca)
+    prediction = loaded_model.predict(flat_data)
     st.write(prediction)
     
 
