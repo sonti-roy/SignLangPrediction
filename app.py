2556e275ce565734396853133418b7d8639e261e
Sonti Roy <sontiroy@gmail.com>
2024-04-27 00:02:34 +0530

    now done

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    ## flatten the image
+    flat_data = cv2_img.flatten()
 
     # transfor the array using pca to select 10 features
     from sklearn.decomposition import PCA
     num_features = 10  # Number of features to extract
     pca = PCA(n_components=num_features)  # PCA for feature extraction
     # Perform PCA to extract 10 features
-    pca.fit(cv2_img)
-    flat_data_pca = pca.transform(cv2_img)
+    pca.fit(flat_data)
+    flat_data_pca = pca.transform(flat_data)
     
     # load the model
     import pickle
